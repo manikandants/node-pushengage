c076656ea565e99bcfdae01a3a845fd726693e32
manikandanxenon <manikandan@wearexenon.com>
2016-09-06 10:28:50 +0530

    send notifications

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 var Pushengage = require('../lib/index');
 var pe = new Pushengage({
-    apiKey: 'L19RDWwhXscrVgAn91Fb0174e19EPzZa'
+    apiKey: '<Your Key Here>'
 });
-/*pe.notifications.send({
+pe.notifications.send({
     notification_title: 'New Appointment',
     notification_message: 'Customer booked new appointment',
     notification_url: 'http://cartisan.in'
 }, function(err, data) {
 
-});*/
+});
 pe.notifications.get({
     
 }, function(err, data) {
